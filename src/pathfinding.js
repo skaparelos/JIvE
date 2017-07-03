1d3484fb065e24d3f256504975b88a50dd722841
skaparelos <skaparelos@gmail.com>
2017-07-03 23:47:02 +0100

    added initial structure for pathfinding class

---
+++ src/pathfinding.js
@@ -0,0 +1,18 @@
+class Pathfinding{
+
+    constructor(){}
+
+    buildPassableGrid(map){
+
+    }
+
+    /**
+     *
+     * @param gridMap - gridMap is the output of buildPassableGrid
+     * @param startPoint
+     * @param endPoint
+     */
+    findPath(gridMap, startPoint, endPoint){
+
+    }
+}
